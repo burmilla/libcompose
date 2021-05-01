7b32e89e0247aaf68bb215f617c58efc1ce532f1
Olli Janatuinen <olli.janatuinen@gmail.com>
2021-05-01 19:35:29 +0300

    Bump up github.com/sirupsen/logrus to 1.8.1

--- script/vendor.sh
+++ script/vendor.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$BASH_SOURCE")/.."
 rm -rf vendor/
 source 'script/.vendor-helpers.sh'
 
-clone git github.com/Sirupsen/logrus v0.9.0
+clone git github.com/sirupsen/logrus v1.8.1
 #clone git github.com/codegangsta/cli 6086d7927ec35315964d9fea46df6c04e6d697c1
 clone git github.com/codegangsta/cli 839f07bfe4819fa1434fa907d0804ce6ec45a5df
 clone git github.com/docker/distribution 467fc068d88aa6610691b7f1a677271a3fac4aac
@@ -23,6 +23,7 @@ clone git github.com/davecgh/go-spew 5215b55f46b2b919f50a1df0eaa5886afe4e3b3d
 clone git github.com/pmezard/go-difflib d8ed2627bdf02c080bf22230dbb337003b7aba2d
 clone git golang.org/x/crypto 4d48e5fa3d62b5e6e71260571bf76c767198ca02 https://github.com/golang/crypto.git
 clone git golang.org/x/net 47990a1ba55743e6ef1affd3a14e5bac8553615d https://github.com/golang/net.git
+clone git golang.org/x/sys bc7a7d42d5c30f4d0fe808715c002826ce2c624e https://github.com/golang/sys.git
 clone git gopkg.in/check.v1 11d3bc7aa68e238947792f30573146a3231fc0f1
 clone git github.com/cloudfoundry-incubator/candiedyaml 5cef21e2e4f0fd147973b558d4db7395176bcd95
 clone git github.com/Azure/go-ansiterm 388960b655244e76e24c75f48631564eaefade62
